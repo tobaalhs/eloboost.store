b55adc8b2724a27066c25565ebc5199cb99b75db
Tobal <84552030+tobaalhs@users.noreply.github.com>
2025-01-07 13:46:41 -0300

    Update script.js

--- ranked-boost/script.js
+++ ranked-boost/script.js
@@ -788,7 +788,7 @@ payButton.addEventListener('click', () => {
     `El precio calculado es: ${precioTexto}%0A%0A` +
     `Por favor, confirma si este es el precio correcto.`;
 
-    const numeroWhatsApp = '+56991991705';
+    const numeroWhatsApp = '+56929169240';
     const urlWhatsApp = `https://wa.me/${numeroWhatsApp}?text=${mensaje}`;
     window.open(urlWhatsApp, '_blank');
 });
